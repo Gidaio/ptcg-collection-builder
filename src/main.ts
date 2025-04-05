184d320241d350235ec8b386d4c54a5dad227651
Gidaio <killshade@gmail.com>
2025-04-05 12:26:18 -0700

    Add partitionON function.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { getSetCards } from "./set-cache.ts";
+import { partitionOn } from "./util.ts";
 
 const cards = await getSetCards("SVI");
+const cardsByRarity = partitionOn(cards, "rarity");
 
-console.log(JSON.stringify(cards, null, 2));
+console.log(cardsByRarity["Common"]?.[0]);
+console.log(cardsByRarity["Uncommon"]?.[0]);
+console.log(cardsByRarity["Rare"]?.[0]);
+console.log(cardsByRarity["Double Rare"]?.[0]);
+console.log(cardsByRarity["Illustration Rare"]?.[0]);
+console.log(cardsByRarity["Ultra Rare"]?.[0]);
+console.log(cardsByRarity["Special Illustration Rare"]?.[0]);
+console.log(cardsByRarity["Hyper Rare"]?.[0]);
--- src/util.ts
+++ src/util.ts
@@ -9,3 +9,20 @@ export async function readTextFileIfExists(filePath: string): Promise<string | n
         }
     }
 }
+
+export function partitionOn<K extends keyof T, T extends { [key in K]: string }>(
+    arr: T[],
+    key: K,
+): Partial<Record<T[K], T[]>> {
+    const out: Partial<Record<T[K], T[]>> = {};
+
+    for (const obj of arr) {
+        if (!(obj[key] in out)) {
+            out[obj[key]] = [obj];
+        } else {
+            out[obj[key]]!.push(obj);
+        }
+    }
+
+    return out;
+}
