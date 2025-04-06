d5f3408009d2b84491965fe6fb3c297a73aa9f29
Gidaio <killshade@gmail.com>
2025-04-05 18:03:25 -0700

    Add command line parameters.

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,36 @@ import { partitionOn } from "./util.ts";
 
 export type CardWithQuantity = Card & { quantity: number };
 
-const cards = await getSetCards("SVI");
+const setToOpen = Deno.args[0];
+if (!setToOpen) {
+    console.warn("Please give a set to open packs from.");
+    Deno.exit(1);
+}
+
+const packsToOpen = Number(Deno.args[1]);
+if (isNaN(packsToOpen)) {
+    console.warn("Please give a number of packs to add to your collection.");
+    Deno.exit(1);
+}
+if (!Number.isInteger(packsToOpen)) {
+    console.warn("Please give an INTEGER number of packs to add to your collection. Dingus.");
+    Deno.exit(1);
+}
+
+const cards = await getSetCards(setToOpen);
 const cardsByRarity = partitionOn(cards, "rarity");
 
 const collection: Collection = {};
 
-addBoosterToCollection(cardsByRarity, collection, {
-    doubleRare: 0.1376,
-    ultraRare: 0.0657,
-    illustrationRare: 0.0767,
-    specialIllustrationRare: 0.0315,
-    hyperRare: 0.0185,
-});
+for (let i = 0; i < packsToOpen; i += 1) {
+    addBoosterToCollection(cardsByRarity, collection, {
+        doubleRare: 0.1376,
+        ultraRare: 0.0657,
+        illustrationRare: 0.0767,
+        specialIllustrationRare: 0.0315,
+        hyperRare: 0.0185,
+    });
+}
 
 const collectionCards = Object.entries(collection)
     .map<CardWithQuantity>(([number, quantity]) => ({
